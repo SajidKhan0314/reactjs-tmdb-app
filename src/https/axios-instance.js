426be1a6eafcf558adbbb08597f2bbe6af33bfdf
SajidKhan0314 <sajid.bsse3299@iiu.edu.pk>
2021-04-20 00:47:25 +0500

    Updates

--- src/https/axios-instance.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-const axiosInstance = axios.create({
-    baseURL: 'https://api.themoviedb.org/3/search/movie?api_key=1115fb0436d21fc0905dbb235b1ae399&query='
-});
-
-export default axiosInstance;
